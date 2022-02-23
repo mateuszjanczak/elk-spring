845f73873f9231502fcac14ba542b59665dc4480
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2022-02-23 23:23:15 +0100

    Init project

---
+++ src/main/kotlin/com/mateuszjanczak/elk/ElkApplication.kt
@@ -0,0 +1,11 @@
+package com.mateuszjanczak.elk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ElkApplication
+
+fun main(args: Array<String>) {
+	runApplication<ElkApplication>(*args)
+}
---
+++ src/test/kotlin/com/mateuszjanczak/elk/ElkApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mateuszjanczak.elk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ElkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
